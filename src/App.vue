e07435c15b19891707749ce6b9a43fda9c6ecd7a
hjnam@raoncorp.com <hjnam@raoncorp.com>
2024-02-19 16:25:00 +0900

    init : git Test

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>Vue TODO with Typescript</h1>
+    <TodoInput :item="todoText"></TodoInput>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoInput from "@/components/TodoInput.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld,
+    TodoInput,
+  },
+  data() {
+    return {
+      todoText: "",
+    };
   },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input type="text" id="todo-input" :value="item" />
+    <button @click="addTodo" type="button">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["item"],
+  methods: {
+    addTodo() {
+      console.log("todo");
+    },
+  },
+});
+</script>
